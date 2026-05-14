cd43fa356e28b3d41d50286b2386609bdd9840ba
anurag-yadav-1619 <anuragyadava07277@gmail.com>
2026-05-14 19:05:13 +0530

    py

---
+++ PYTHON (AI school)/tuples.py
@@ -0,0 +1,42 @@
+# TUPLE IS A IN MUATABLE WHER ELIST IS A MUSTABLE
+# a=(1,2,3,4,5,6)
+# print(type(a))
+# print(a[1])
+# print(a[2])
+# print(a[3])
+# print(a[4])
+
+# ti is hatroginus in nature
+
+# a=(1,2,3,4,5,print("sujal"),"anurag")
+# for i in range(len(a)):
+#     print(a[i])
+
+# a=(1,2,3,4,5,6,7,9)
+# index=a.index(5)
+# print(index)
+# count=a.count(5)
+# print(count)
+
+a,b,c,d=(1,2,3,4)
+print(type(a))
+print(b)
+print(c)
+print(d)
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
---
+++ set.py
@@ -0,0 +1,23 @@
+# s={1,2,3,4,5,63,4,5}
+# print(s)
+
+# b=hash("hello")
+# print(b)
+# c=hash((1,))
+
+# a={1,2,3,4}
+# a.remove(1)
+# a.discard(2)
+# a.pop()
+# a.clear()
+# print(a)
+
+# a={1,2,3,4,5}
+# b={6,7,8,9}
+# s=a.union(b)
+# s=a|b
+# s=a.intersection(b)
+# s=a&(b)
+# s=b.difference(a)
+# print(s)
+
